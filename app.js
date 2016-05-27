471b826c9e2140cf590ddd8c18b6009b8afc9487
Tim Park <timothsp@uci.edu>
2016-05-27 14:35:51 -0700

    Changed app.js to serve from /dist

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/app'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res){
 	res.sendFile('/index.html');
