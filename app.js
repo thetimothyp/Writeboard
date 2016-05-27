2eda85ace73dd5bb1052c75b4f3cae72674de742
Tim Park <timothsp@uci.edu>
2016-05-27 14:46:02 -0700

    Port tweaking

--- app.js
+++ app.js
@@ -22,6 +22,6 @@ io.on('connection', function(socket) {
 	})
 })
 
-http.listen(3000, function() {
-	console.log('listening on  port 3000');
+http.listen(process.env.PORT || 3000, function() {
+	console.log('listening on  port ' + process.env.PORT);
 })
