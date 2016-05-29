bc9dade49c6cd8ea01fb30560482fdbfecd4fc70
Tim Park <timothsp@uci.edu>
2016-05-29 10:45:18 -0700

    Housekeeping

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
+// Initializations
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io').listen(http);
-
 var mongoose = require('mongoose');
+
+// Connect to MongoDB
 mongoose.connect('mongodb://test:test@ds013908.mlab.com:13908/writeboard-app');
 
+// Create Channel collection
 var Channel = require('./schema.js');
 
+// Set up application routing
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req, res){
 	res.sendFile('/index.html');
 })
 
+// Listen for socket connections from clients
 io.on('connection', function(socket) {
 	var currentRoom = 'all';
 	console.log('user connected');
 	socket.on('disconnect', function() {
 		console.log('user disconnected');
 	})
+	// On 'join' event, switch socket client to given room and emit chat 
+	// history of that room to the client
 	socket.on('join', function(room) {
 		socket.join(room);
 		currentRoom = room;
@@ -35,12 +42,15 @@ io.on('connection', function(socket) {
 	socket.on('leave', function(room) {
 		socket.leave(room);
 	})
+	// On receiving 'chat' from client, broadcast the message to the whole 
+	// room and add the message to the database
 	socket.on('chat', function(msg) {
 		io.to(currentRoom).emit('chat', msg);
 		Channel.findOneAndUpdate({ name: currentRoom }, { $push: { messages: msg }}, { upsert: true }, function(err) {
 			if (err) console.log(err);
 		});
 	})
+	// Broadcast 'draw' command to all room participants
 	socket.on('drawClick', function(data) {
 		socket.broadcast.to(currentRoom).emit('draw', { x : data.x, y : data.y, type: data.type });
 	})
